b002cf772c54529a0d46750a48476829303a1c36
CarlosZeyy <carloszeeyy@gmail.com>
2026-03-16 19:57:46 -0300

    Add: Refactor input handling for matrix creation and add new matrix example

--- Aula 09-03-2026/matriz.py
+++ Aula 09-03-2026/matriz.py
@@ -1,22 +1,11 @@
 m = []
-v1 = []
-v2 = []
-v3 = []
 
 for i in range(3):
-    valor = int(input("Digite um valor para o vetor 1: "))
-    v1.append(valor)
-m.append(v1)
-
-for i in range(3):
-    valor2 = int(input("Digite um valor para o vetor 2: "))
-    v2.append(valor2)
-m.append(v2)    
-
-for i in range(3):
-    valor3 = int(input("Digite um valor para o vetor 3: "))
-    v3.append(valor3)
-m.append(v3)
+    vetor = []
+    for j in range(3):
+        valor = int(input(f'Digite um valor para o vetor {i + 1}, posição {j + 1}: '))
+        vetor.append(valor)
+    m.append(vetor)
 
 for linha in m:
     print(linha)
