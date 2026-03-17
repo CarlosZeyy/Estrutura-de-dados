24d225c56550fea172f545d25e85129691ff937d
CarlosZeyy <carloszeeyy@gmail.com>
2026-03-16 21:09:19 -0300

    Add: Queue and stacks exemples

---
+++ Aula 16-03-2026/Stacks.py
@@ -0,0 +1,10 @@
+pilhaDePrato = []
+
+pilhaDePrato.append('Prato Branco')
+print(pilhaDePrato)
+
+pilhaDePrato.append('Prato Azul')
+print(pilhaDePrato)
+
+pilhaDePrato.pop()
+print(pilhaDePrato)
---
+++ Aula 16-03-2026/stack1.py
@@ -0,0 +1,8 @@
+s = []
+
+for i in range(3):
+    valor = int(input("Digite um valor: "))
+    s.append(valor)
+print(s)
+s.pop()
+print(s)
